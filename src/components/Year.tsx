d5106ad7882b7376a37e878e303a71eb8d036dce
Futa HIRAKOBA <korosuke613613@gmail.com>
2022-08-21 00:13:59 +0900

    feat: filter by years

---
+++ src/components/Year.tsx
@@ -0,0 +1,23 @@
+import path from 'path';
+
+import { AppConfig } from '@/utils/AppConfig';
+
+type IYearProps = {
+  year: string;
+  contentCategory: string;
+};
+
+export const Year = (props: IYearProps) => {
+  return (
+    <a
+      href={path.join(
+        AppConfig.base,
+        props.contentCategory.toLowerCase(),
+        'year',
+        props.year
+      )}
+    >
+      <div className={`px-1 py-0.5 text-sm font-semibold`}>{props.year}</div>
+    </a>
+  );
+};
---
+++ src/utils/Year.ts
@@ -0,0 +1,17 @@
+import fs from 'node:fs';
+
+export const readYears = async () => {
+  const yearsFile = await fs.promises.readFile('./build/years.json');
+  const years = JSON.parse(yearsFile.toString()) as {
+    [key: string]: number[];
+    posts: number[];
+    projects: number[];
+    blogs: number[];
+  };
+
+  Object.keys(years).forEach((category) => {
+    years[category]?.sort((a, b) => b - a);
+  });
+
+  return years;
+};
