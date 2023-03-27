92700b0b46972c0be705e8d57321ee9392fa5f04
Futa HIRAKOBA <korosuke613613@gmail.com>
2023-03-28 02:12:38 +0900

    fix: setting rss for content collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,7 +3,7 @@ import { defineCollection, z } from 'astro:content';
 const postCollection = defineCollection({
   schema: z.object({
     title: z.string(),
-    description: z.string().nullable().optional(),
+    description: z.string().optional(),
     pubDate: z.date(),
     tags: z.array(z.string()),
     order: z.number().optional(),
--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,20 +1,32 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss from '@astrojs/rss';
+import type { APIContext } from 'astro';
+import { getCollection } from 'astro:content';
 
 import { AppConfig } from '@/utils/AppConfig';
 
-export const get = async () =>
-  rss({
+export const get = async (context: APIContext) => {
+  const posts = await getCollection('posts');
+  const { site } = context;
+  if (site === undefined) {
+    throw new Error('context.site is undefined');
+  }
+
+  return rss({
     // `<title>` field in output xml
     title: AppConfig.title,
     // `<description>` field in output xml
     description: AppConfig.description,
     // base URL for RSS <item> links
     // SITE will use "site" from your project's astro.config.
-    site: import.meta.env.SITE,
+    site: site.toString(),
     // list of `<item>`s in output xml
     // simple example: generate items for every md file in /src/pages
     // see "Generating items" section for required frontmatter and advanced use cases
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    items: posts.map((post) => ({
+      ...post.data,
+      link: `/posts/${post.slug}`,
+    })),
     // (optional) inject custom xml
     customData: `<language>ja-jp</language>`,
   });
+};
