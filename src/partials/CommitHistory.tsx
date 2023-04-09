7ec46b99681ee870bedcbc226294a83c02251079
Futa HIRAKOBA <korosuke613613@gmail.com>
2023-04-09 22:16:58 +0900

    feat: add commit histories for posts

---
+++ src/partials/CommitHistory.tsx
@@ -0,0 +1,49 @@
+import { ExternalLink } from '@/components/ExternalLink';
+import { AppConfig } from '@/utils/AppConfig';
+import type { CommitHistory } from '@/utils/CommitHistories';
+
+type ICommitHistoryPost = {
+  collection: string;
+  id: string;
+  histories: CommitHistory[];
+};
+
+export const CommitHistoryPost = (props: ICommitHistoryPost) => {
+  const url =
+    props.id === 'dormitory_introduction'
+      ? // dormitory_introduction: https://github.com/korosuke613/homepage-2nd/commits/main/src/pages/posts/dormitory_introduction.astro
+        `https://${AppConfig.github_url}/commits/main/src/pages/${props.collection}/${props.id}.astro`
+      : // normal: https://github.com/korosuke613/homepage-2nd/blob/main/src/content/posts/20200324_graduate_miyazaki_u.md
+        `https://${AppConfig.github_url}/commits/main/src/content/${props.collection}/${props.id}`;
+
+  return (
+    <div>
+      <h3 className="my-4 text-xl font-bold">編集履歴</h3>
+      <ul className="list-disc">
+        {props.histories
+          .map((h) => {
+            // 2023-04-05T19:05:58+09:00 を 2023-04-05 に整形する
+            const dateWithoutTime = h.date.split('T')[0];
+
+            return (
+              <li key={h.sha} className="list-inside pt-1 text-sm">
+                <span>
+                  <span>
+                    <code>{dateWithoutTime}</code>:{' '}
+                  </span>{' '}
+                  <ExternalLink
+                    title={h.commitMessage}
+                    url={`https://${AppConfig.github_url}/commit/${h.sha}`}
+                  />
+                </span>
+              </li>
+            );
+          })
+          .slice(0, 6)}
+        <li className="m-0 list-inside pt-1 text-sm">
+          <ExternalLink title="全て表示" url={url} />
+        </li>
+      </ul>
+    </div>
+  );
+};
---
+++ src/utils/CommitHistories.ts
@@ -0,0 +1,25 @@
+import { simpleGit } from 'simple-git';
+
+export type CommitHistory = {
+  sha: string;
+  date: string;
+  commitMessage: string;
+};
+
+const MAX_COUNT = 3;
+
+export const getCommitHistories = async (
+  filePath: string
+): Promise<CommitHistory[]> => {
+  const git = simpleGit();
+  const log = await git.log({
+    file: filePath,
+    maxCount: MAX_COUNT,
+  });
+
+  return log.all.map((l) => ({
+    sha: l.hash,
+    date: l.date,
+    commitMessage: l.message,
+  }));
+};
