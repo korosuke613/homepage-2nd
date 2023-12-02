227262417c823668b002fb4f9fd5b5ad9cdee12e
Futa HIRAKOBA <korosuke613613@gmail.com>
2023-12-02 19:56:42 +0900

    refactor: move SocialLinks

--- src/components/SocialLinks/SocialLinks.stories.tsx
+++ src/components/SocialLinks/SocialLinks.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { SocialLinks } from "./SocialLinks";
+import { SocialLinks } from ".";
 
 const metaData: Meta = {
   title: "SocialLinks",
