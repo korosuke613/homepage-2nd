34d5d5cdcccd638adda809ed209c3046b7fa7861
Futa HIRAKOBA <korosuke613613@gmail.com>
2023-12-02 17:28:59 +0900

    ci: init storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,32 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  viteFinal: async (config) => {
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve?.alias,
+          // Add your own alias like this
+          "@": "/src",
+        },
+      },
+    };
+  },
+};
+export default config;
