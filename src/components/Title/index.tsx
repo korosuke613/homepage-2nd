407ea83428ede2ef4e3f5628fa62baa10f0211f7
Futa HIRAKOBA <korosuke613613@gmail.com>
2024-05-05 01:14:29 +0900

    fix: broken title link

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -1,26 +1,12 @@
-import { AppConfig } from "@/utils/AppConfig";
-
 type ITitleProps = {
   preTitle: string | number | undefined;
   contentCategory: string;
 };
 
 export const Title: React.FC<ITitleProps> = ({ preTitle, contentCategory }) => {
-  const linkedContentCategory = (
-    <a href={`${AppConfig.base}/${contentCategory.toLowerCase()}`}>
-      {contentCategory}
-    </a>
-  );
-
   if (preTitle === undefined) {
-    return linkedContentCategory;
+    return contentCategory;
   }
 
-  return (
-    <>
-      <span>
-        {preTitle} / {linkedContentCategory}
-      </span>
-    </>
-  );
+  return `${preTitle} / ${contentCategory}`;
 };
